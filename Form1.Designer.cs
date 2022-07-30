b604e20f4377917739eebd73ed14fb7202f8ab94
Carin Esther Sanchéz Morales <USSS107522@ugb.edu.sv>
2022-07-29 19:00:38 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace semana2_formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nombre1 = new System.Windows.Forms.Label();
+            this.nombre2 = new System.Windows.Forms.Label();
+            this.nombre3 = new System.Windows.Forms.Label();
+            this.nombre4 = new System.Windows.Forms.Label();
+            this.nombre5 = new System.Windows.Forms.Label();
+            this.nombre6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Integrantes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nombre1
+            // 
+            this.nombre1.AutoSize = true;
+            this.nombre1.Location = new System.Drawing.Point(156, 167);
+            this.nombre1.Name = "nombre1";
+            this.nombre1.Size = new System.Drawing.Size(0, 13);
+            this.nombre1.TabIndex = 1;
+            this.nombre1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nombre2
+            // 
+            this.nombre2.AutoSize = true;
+            this.nombre2.Location = new System.Drawing.Point(156, 226);
+            this.nombre2.Name = "nombre2";
+            this.nombre2.Size = new System.Drawing.Size(0, 13);
+            this.nombre2.TabIndex = 2;
+            this.nombre2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // nombre3
+            // 
+            this.nombre3.AutoSize = true;
+            this.nombre3.Location = new System.Drawing.Point(156, 273);
+            this.nombre3.Name = "nombre3";
+            this.nombre3.Size = new System.Drawing.Size(0, 13);
+            this.nombre3.TabIndex = 3;
+            // 
+            // nombre4
+            // 
+            this.nombre4.AutoSize = true;
+            this.nombre4.Location = new System.Drawing.Point(412, 167);
+            this.nombre4.Name = "nombre4";
+            this.nombre4.Size = new System.Drawing.Size(0, 13);
+            this.nombre4.TabIndex = 4;
+            // 
+            // nombre5
+            // 
+            this.nombre5.AutoSize = true;
+            this.nombre5.Location = new System.Drawing.Point(412, 226);
+            this.nombre5.Name = "nombre5";
+            this.nombre5.Size = new System.Drawing.Size(0, 13);
+            this.nombre5.TabIndex = 5;
+            this.nombre5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // nombre6
+            // 
+            this.nombre6.AutoSize = true;
+            this.nombre6.Location = new System.Drawing.Point(412, 273);
+            this.nombre6.Name = "nombre6";
+            this.nombre6.Size = new System.Drawing.Size(0, 13);
+            this.nombre6.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 53);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Cerrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(456, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 40);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Borrar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.nombre6);
+            this.Controls.Add(this.nombre5);
+            this.Controls.Add(this.nombre4);
+            this.Controls.Add(this.nombre3);
+            this.Controls.Add(this.nombre2);
+            this.Controls.Add(this.nombre1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label nombre1;
+        private System.Windows.Forms.Label nombre2;
+        private System.Windows.Forms.Label nombre3;
+        private System.Windows.Forms.Label nombre4;
+        private System.Windows.Forms.Label nombre5;
+        private System.Windows.Forms.Label nombre6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace semana2_formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nombre1.Text = "Carin Sanchez";
+            nombre2.Text = "Xiomara Machado";
+            nombre3.Text = "Patricia Hernandez";
+            nombre4.Text = "Elias Gevarra";
+            nombre5.Text = " Isaac Nolasco";
+            nombre6.Text = " Cindy Romero ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nombre1.Text = " ";
+            nombre2.Text = " ";
+            nombre3.Text = " ";
+            nombre4.Text = " ";
+            nombre5.Text = " ";
+            nombre6.Text = " ";
+        }
+    }
+}
